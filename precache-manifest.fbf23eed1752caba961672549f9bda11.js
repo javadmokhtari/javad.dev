bf8546beecec211f222807368d6e1efec4a6905e
javadmokhtari <javadmokhtari@outlook.com>
2020-03-13 22:02:46 +0330

    Updates

---
+++ precache-manifest.fbf23eed1752caba961672549f9bda11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cab9faf39b014125a39144b7f2fa20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26041ef329099f8803f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3878601de0d45d14412",
+    "url": "/static/js/2.2bdb478e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2bdb478e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26041ef329099f8803f4",
+    "url": "/static/js/main.edb2f7e7.chunk.js"
+  },
+  {
+    "revision": "f8450e4e643734c25ac0",
+    "url": "/static/js/runtime-main.b0e826de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
