4bc42e1f9bca34317742e6235254239607b64d21
javadmokhtari <javadmokhtari@outlook.com>
2020-03-13 22:32:15 +0330

    added

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Layout extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return (
+            <div>
+                Test
+                {this.props.children}
+            </div>
+        )
+    }
+}
